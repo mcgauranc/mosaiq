cde4d782c7267ec6cdc48975f70ceed95bb94dc4
Ciaran McGauran <Ciaran_McGauran@rapid7.com>
2016-12-29 21:21:34 +0000

    DB Update

--- activflow/settings.py
+++ activflow/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'django_extensions',
+    'django.contrib.flatpages',
     'activflow.core',
     'activflow.tests'
 )
